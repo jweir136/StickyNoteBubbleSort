b2fbe43b0c6a4a069d46932a78ed6ed7c4c17eac
jweir136 <31812897+jweir136@users.noreply.github.com>
2020-09-30 12:56:40 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include "StickyNote.h"
+//#include "Sort.h"
+
+using namespace std;
+
+vector<StickyNote> vec;
+
+///// Init the vector /////
+
+void init() {
+  for (int i = 0; i < 10; i++) {
+    StickyNote *Note = new StickyNote(10 - i);
+    vec.insert(vec.begin() + i, *Note);
+  }
+}
+
+////// print the vector elements /////
+
+void printVector() {
+  for (int i = 0; i < vec.size(); i++)
+    cout << vec[i].getValue() << endl;
+}
+
+int main() {
+  init();
+  bubbleSort(vec);
+}
